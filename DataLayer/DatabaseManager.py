ccd6e66f90e68c9c0bb52f3bcbc5736d094724d4
alirezaf2001 <alireza.f220@gmail.com>
2022-12-01 23:56:21 +0330

    add database manager to data layer 1/12/2022 11:56 pm

---
+++ DataLayer/DatabaseManager.py
@@ -0,0 +1,19 @@
+import pyodbc
+
+SERVER = 'DESKTOP-S2P20AR'
+DATABASE = 'Traffic_Fines'
+
+# SERVER = servername
+# DATABASE = databasename
+
+connection_string = f'DRIVER={{ODBC Driver 17 for SQL Server}};SERVER={SERVER};DATABASE={DATABASE};Trusted_Connection=yes;'
+connection = pyodbc.connect(connection_string)
+
+class dbManage():
+    def __init__(self) -> None:
+        self.cursor = connection.cursor()
+
+    def exeQuery(self, query):
+        self.cursor.execute(query)      
+        data = self.cursor.fetchall()
+        return data

0f5509ef0d9ec20b3ccfbe65099f8cbfb203f004
pr1 <pr2u@example.com>
2022-12-25 12:17:03 -0800

    ver3

--- LogicLayer/LL_tblVehicle.py
+++ LogicLayer/LL_tblVehicle.py
@@ -1,13 +1,19 @@
-def select_all(self):
-    data = self.db.exeQuery("SELECT * FROM tblPerson")
-    return data    
-def update_(self):
-    self.db.exeQuery("UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'")
+from ..DataLayer import DatabaseManager
+
+class tblVehicle_logiclayer:
+    def __init__(self) -> None:
+        self.db = DatabaseManager.dbManage()
+
+    def select_all(self):
+        data = self.db.exeQuery("SELECT * FROM tblPerson")
+        return data    
+    def update_(self):
+        self.db.exeQuery("UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'")
          
-def insert(self):
-    self.db.exeQuery("INSERT INTO customers (name, address) VALUES (%s, %s)")
+    def insert(self):
+        self.db.exeQuery("INSERT INTO customers (name, address) VALUES (%s, %s)")
         
                
-def delete_(self):
-    self.db.exeQuery("DELETE FROM customers WHERE address = 'Mountain 21'")
+    def delete_(self):
+        self.db.exeQuery("DELETE FROM customers WHERE address = 'Mountain 21'")
         

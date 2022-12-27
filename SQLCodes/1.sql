ce77f9a0f8a0e37b8ef970a09fd2f10d480b085e
pr1 <pr2u@example.com>
2022-12-27 12:00:13 -0800

    ver6

---
+++ SQLCodes/1.sql
@@ -0,0 +1,24 @@
+create procedure insertuser
+(
+@I int,
+@First nvarchar(50),
+@Last nvarchar(50)
+)
+as
+begin
+insert into tblPerson(Id,FirstName,LastName)
+end 
+
+create procedure insertfine
+(
+@id nvarchar(50),
+@Plate nvarchar(50),
+@Date Datetime,
+@FType nvarchar(50),
+@cost int
+
+)
+as
+begin
+insert into tblFine(Id,PlateNum,Date,FineType,Cost)
+end

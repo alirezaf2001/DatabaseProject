72a0d47c221fd458788c116bf6b01efb669fac09
pr1 <pr2u@example.com>
2022-12-27 13:48:16 -0800

    ver 9-2-1

--- LogicLayer/LL_tblFine.py
+++ LogicLayer/LL_tblFine.py
@@ -5,7 +5,7 @@ class tblFine_LogicLayer:
         self.db.exeQuery(f"select * from tblFine where id='{id }' and fromdate='{fromdate }' and todate <'{ todate}'")
         
     def fname(self,First,Last,Plate,date,FType,cost):
-        self.db.exeQuery(f"select * from tblFine where firstname='{ First}' and lastname='{ Last}' and plate='{ Plate}' and finetype='{FType}' and cost='{}'")
+        self.db.exeQuery(f"select * from tblFine where firstname='{ First}' and lastname='{ Last}' and plate='{ Plate}' and finetype='{FType}' and cost='{cost}'")
    
     
     def fname(arg):

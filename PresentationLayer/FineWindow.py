2199212f0816e22dc821addf64e4e38478ac7e1d
alirezaf2001 <alireza.f220@gmail.com>
2022-12-27 20:06:27 +0330

    fixed few problem fixing entry names 8:06pm 12/27/2022

--- PresentationLayer/FineWindow.py
+++ PresentationLayer/FineWindow.py
@@ -11,20 +11,20 @@ class Window:
         lable = Label(frame, text="کد ملی:")
         lable.pack(side='left', anchor='nw',padx=5,pady=5,ipadx=20,ipady=10)
 
-        entry = Entry(frame)
-        entry.pack(side='left', anchor='nw',padx=5,pady=5,ipadx=20,ipady=10)
+        entry_id = Entry(frame)
+        entry_id.pack(side='left', anchor='nw',padx=5,pady=5,ipadx=20,ipady=10)
 
         lable = Label(frame, text="از تاریخ:")
         lable.pack(side='left', anchor='nw',padx=5,pady=5,ipadx=20,ipady=10)
 
-        entry = Entry(frame)
-        entry.pack(side='left', anchor='nw',padx=5,pady=5,ipadx=20,ipady=10)
+        entry_fromDate = Entry(frame)
+        entry_fromDate.pack(side='left', anchor='nw',padx=5,pady=5,ipadx=20,ipady=10)
 
         lable = Label(frame, text="تا تاریخ:")
         lable.pack(side='left', anchor='nw',padx=5,pady=5,ipadx=20,ipady=10)
 
-        entry = Entry(frame)
-        entry.pack(side='left', anchor='nw',padx=5,pady=5,ipadx=20,ipady=10)
+        entry_toDate = Entry(frame)
+        entry_toDate.pack(side='left', anchor='nw',padx=5,pady=5,ipadx=20,ipady=10)
 
         cols = ('id','plateNum','date','fineType','cost')
         self.treeview = Treeview(master, columns=cols,selectmode='browse', show= 'headings')
@@ -55,6 +55,7 @@ class Window:
         button = Button(master, text="خروج",style='btnStyle.TButton',command=master.destroy)
         button.pack(side='bottom', anchor='se',padx=5,pady=5,ipadx=50,ipady=10)
 
+    #TODO:Check if the select works
     def loadData(self):
         vehicle = LL_tblVehicle.tblVehicle_LogicLayer()
         data = vehicle.select_all()
@@ -93,5 +94,5 @@ class MainWindow:
     def startWindow(self):
         self.configStyle()
         self.root.title('Persons')
-        self.root.geometry("800x500")
+        self.root.geometry("1000x600")
         self.root.mainloop()

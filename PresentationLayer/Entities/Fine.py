ef5dda266570ee2bba656225f8d0c98b0f4490fe
alirezaf2001 <alireza.f220@gmail.com>
2022-12-30 01:07:39 +0330

    add all Entites

---
+++ PresentationLayer/Entities/Fine.py
@@ -0,0 +1,7 @@
+class Fine:
+    def __init__(self, Id, PlateNum, Date, FineType, Cost) -> None:
+        self.Id = Id
+        self.PlateNum = PlateNum
+        self.Date = Date
+        self.FineType = FineType
+        self.Cost = Cost
